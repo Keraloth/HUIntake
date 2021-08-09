79d431f64f1c50e0b0d8cfd468df498ff14c8b7b
Keraloth <denise-vandoorn@hotmail.nl>
2021-08-09 17:16:44 +0200

    Update assignment01vandoorn.py
    oreo cookies

--- assignment01vandoorn.py
+++ assignment01vandoorn.py
@@ -1,10 +1,3 @@
-#calculate how much one Oreo cookie is concerning: calories, sodium, carbohydrate, fat 
-#create a user input that asks how much cookies you ate 
-#calculate how much calories, etc. you consumed 
-#warn the user that if he/she surpasses 2000kcal he/she should stop eating these darn delicious cookies 
-#use variables! 
-#save the file as assignment01yourlastname.py
-
 calories= 160
 calories_cookie = 53.33
 total_fat = 7
